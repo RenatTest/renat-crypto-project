7fd1da7588d8486be5698a9ef6a1cdeded9dae5b
RenatTest <renattesttask@gmail.com>
2025-10-21 17:38:39 +0300

    feat(RCP-cryprolist): added refresh indicator to the coins list

--- lib/features/crypto_list/presentation/ui/screens/crypto_list_page.dart
+++ lib/features/crypto_list/presentation/ui/screens/crypto_list_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: lines_longer_than_80_chars
 
+import 'package:easy_refresh/easy_refresh.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -25,81 +26,96 @@ class CryptoListPage extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: BlocBuilder<CryptoListCubit, CryptoListState>(
-          builder: (context, state) {
-            return switch (state.status) {
-              CryptoListStatus.initial => const Center(
-                child: Text('Crypto List data', style: TextStyle(fontSize: 24)),
-              ),
-
-              CryptoListStatus.loading => const Center(
-                child: CircularProgressIndicator(),
-              ),
-
-              CryptoListStatus.loaded => Center(
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    maxWidth: kIsWeb ? 400 : double.infinity,
-                  ),
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: state.cryptoList?.cryptoList?.length,
-                          itemBuilder: (context, index) {
-                            final coin =
-                                state.cryptoList?.cryptoList?[index]['coin']
-                                    .toString() ??
-                                'Coin';
-
-                            final price =
-                                state.cryptoList?.cryptoList?[index]['price']
-                                    .toStringAsFixed(5)
-                                    .toString() ??
-                                '0';
-
-                            final image =
-                                state
-                                    .cryptoList
-                                    ?.cryptoList?[index]['coinImage']
-                                    .toString() ??
-                                'Coin image';
-
-                            final changePrice =
-                                (((state.cryptoList?.cryptoList?[index]['price'] -
-                                                state
-                                                    .cryptoList
-                                                    ?.cryptoList?[index]['priceOpen24Hour']) /
-                                            state
-                                                .cryptoList
-                                                ?.cryptoList?[index]['priceOpen24Hour']) *
-                                        100)
-                                    .toStringAsFixed(2)
-                                    .toString();
-
-                            return CryptoCoinCard(
-                              coinName: coin,
-                              coinPrice: price,
-                              coinImage: image,
-                              coinChange: changePrice,
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-
-              CryptoListStatus.error => Center(
-                child: Text(
-                  state.errorMessage ?? 'Error',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontSize: 24),
-                ),
-              ),
-            };
+        child: EasyRefresh(
+          header: const MaterialHeader(
+            color: Colors.white,
+            backgroundColor: Colors.deepOrange,
+          ),
+          onRefresh: () async {
+            await context.read<CryptoListCubit>().getCryptoList();
           },
+          child: BlocBuilder<CryptoListCubit, CryptoListState>(
+            builder: (context, state) {
+              return switch (state.status) {
+                CryptoListStatus.initial => const Center(
+                  child: Text(
+                    'Crypto List data',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+
+                CryptoListStatus.loading => const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.deepOrange,
+                    backgroundColor: Colors.white,
+                  ),
+                ),
+
+                CryptoListStatus.loaded => Center(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      maxWidth: kIsWeb ? 400 : double.infinity,
+                    ),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: state.cryptoList?.cryptoList?.length,
+                            itemBuilder: (context, index) {
+                              final coin =
+                                  state.cryptoList?.cryptoList?[index]['coin']
+                                      .toString() ??
+                                  'Coin';
+
+                              final price =
+                                  state.cryptoList?.cryptoList?[index]['price']
+                                      .toStringAsFixed(5)
+                                      .toString() ??
+                                  '0';
+
+                              final image =
+                                  state
+                                      .cryptoList
+                                      ?.cryptoList?[index]['coinImage']
+                                      .toString() ??
+                                  'Coin image';
+
+                              final changePrice =
+                                  (((state.cryptoList?.cryptoList?[index]['price'] -
+                                                  state
+                                                      .cryptoList
+                                                      ?.cryptoList?[index]['priceOpen24Hour']) /
+                                              state
+                                                  .cryptoList
+                                                  ?.cryptoList?[index]['priceOpen24Hour']) *
+                                          100)
+                                      .toStringAsFixed(2)
+                                      .toString();
+
+                              return CryptoCoinCard(
+                                coinName: coin,
+                                coinPrice: price,
+                                coinImage: image,
+                                coinChange: changePrice,
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                CryptoListStatus.error => Center(
+                  child: Text(
+                    state.errorMessage ?? 'Error',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                ),
+              };
+            },
+          ),
         ),
       ),
     );
